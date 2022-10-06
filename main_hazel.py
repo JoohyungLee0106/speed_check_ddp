5ee3f69eeeb0f6dfec6a844359a8c4e2ef7a0d5a
JoohyungLee0106 <liberaboit@hotmail.com>
2022-10-06 14:38:08 +0900

    1006

--- main_hazel.py
+++ main_hazel.py
@@ -1,5 +1,5 @@
 # Edit by Joohyung Lee (JoohyungLee0106)
-# ln 180 부터 보셔도 됩니다.
+# ln 183 부터 보셔도 됩니다.
 
 
 # DDP command (by default, use 3 & 4 gpus):
@@ -216,29 +216,22 @@ def main_worker(gpu, ngpus_per_node, args):
 
     cudnn.benchmark = True
     
-    # Data loading code for ImageNet
-    # traindir = os.path.join(args.data, 'train')
-    # valdir = os.path.join(args.data, 'val')
-    # ImageNet
-    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    # CIFAR-10
-    # training set 전체의 statistic 사용
-    normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
-
     train_dataset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=
         transforms.Compose([
             transforms.RandomResizedCrop(32),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            normalize,
+            # training set 전체의 statistic 사용
+            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
         ]))
 
     val_dataset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=
         transforms.Compose([
             transforms.ToTensor(),
-            normalize,
+            # training set 전체의 statistic 사용
+            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
         ]))
 
     if args.distributed:
@@ -249,7 +242,7 @@ def main_worker(gpu, ngpus_per_node, args):
         val_sampler = None
 
     train_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
         num_workers=args.workers, pin_memory=True, sampler=train_sampler)
 
     val_loader = torch.utils.data.DataLoader(
@@ -340,14 +333,14 @@ def train(train_loader, model, criterion, optimizer, epoch, scaler, scheduler, a
         losses.update(loss.item(), images.size(0))
         top1.update(acc1[0], images.size(0))
         top5.update(acc5[0], images.size(0))
-
-        # compute gradient and do SGD step
-        # optimizer.zero_grad()
+        
+        optimizer.zero_grad()
         # loss.backward()
-        # optimizer.step()
         scaler.scale(loss).backward()
+        # optimizer.step()
         scaler.step(optimizer)
         scaler.update()
+
         scheduler.step()
 
         # measure elapsed time
@@ -370,9 +363,10 @@ def validate(val_loader, model, criterion, args):
                 if torch.cuda.is_available():
                     target = target.cuda(args.gpu, non_blocking=True)
 
-                # compute output
-                output = model(images)
-                loss = criterion(output, target)
+                with torch.cuda.amp.autocast():
+                    # compute output
+                    output = model(images)
+                    loss = criterion(output, target)
 
                 # measure accuracy and record loss
                 acc1, acc5 = accuracy(output, target, topk=(1, 5))
